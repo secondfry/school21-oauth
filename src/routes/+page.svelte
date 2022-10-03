978a2a24dda6505f34f38c73216613efffb9615a
Rustam SecondFry Gubaydullin <secondfry+github@gmail.com>
2022-10-04 02:57:31 +0300

    Display some helpful texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,18 @@
     <pre>{JSON.stringify(data.auth, null, 2)}</pre>
   {:else}
     <a href="/api/auth/signin">Аутентификация</a>
+    <p>
+      В случае использования аутентификации по email, указывайте вашу почту в
+      домене <code>student.21-school.ru</code>.<br />
+      К примеру, я бы указал <code>oadhesiv@student.21-school.ru</code>.
+    </p>
   {/if}
+  <h2>Регистрация нового OAuth клиента в виде Вашего приложения</h2>
+  <p>...делается сейчас вручную, приходите в личку в Телеграме.</p>
+  <hr />
+  Rustam SecondFry Gubaydullin,
+  <a href="https://github.com/secondfry/school21-oauth">исходники</a>,
+  <a href="https://t.me/randomcoffeefrybot">участвуй в случайном кофе</a>!
 </div>
 
 <style lang="scss">
